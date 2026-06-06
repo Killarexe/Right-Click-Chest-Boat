a94813c26eec2c047919359d194904f48f6ffad6
Killar.exe <killar.exe@gmail.com>
2026-06-06 16:37:48 +0200

    RCCB 1.0.5 Release

--- common/src/main/java/github/killarexe/right_click_chest_boat/RCCB.java
+++ common/src/main/java/github/killarexe/right_click_chest_boat/RCCB.java
@@ -3,9 +3,12 @@ package github.killarexe.right_click_chest_boat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Constants {
-
+public class RCCB {
 	public static final String MOD_ID = "right_click_chest_boat";
 	public static final String MOD_NAME = "Right Click Chest Boat";
-	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+	public static void init() {
+		LOGGER.info("Right Click Chest Boat initialized!");
+	}
 }
--- neoforge/src/main/java/github/killarexe/right_click_chest_boat/RCCBMod.java
+++ neoforge/src/main/java/github/killarexe/right_click_chest_boat/RCCBMod.java
@@ -3,18 +3,9 @@ package github.killarexe.right_click_chest_boat;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
-@Mod(Constants.MOD_ID)
+@Mod(RCCB.MOD_ID)
 public class RCCBMod {
-
-    public RCCBMod(IEventBus eventBus) {
-
-        // This method is invoked by the NeoForge mod loader when it is ready
-        // to load your mod. You can access NeoForge and Common code in this
-        // project.
-
-        // Use NeoForge to bootstrap the Common mod.
-        Constants.LOG.info("Hello NeoForge world!");
-        CommonClass.init();
-
+    public RCCBMod(IEventBus _bus) {
+        RCCB.init();
     }
 }
